e62bad051a07381133bcf3245702151d67ce21dd
BaoYibo <1136499432@qq.com>
2019-09-02 18:28:59 +0800

    [新增]-第一次上传

---
+++ miniprogram-1/pages/login/login.js
@@ -0,0 +1,96 @@
+// pages/login/login.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name: '',
+    passWord: ''
+  },
+  toTab(){
+    wx.switchTab({
+      url: '../index/index'
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.showModal({
+
+      title: '标题',
+
+      content: '告知当前状态，信息和解决方法',
+
+      confirmText: '主操作',
+
+      cancelText: '次要操作',
+
+      success: function (res) {
+
+        if (res.confirm) {
+
+          console.log('用户点击主操作')
+
+        } else if (res.cancel) {
+
+          console.log('用户点击次要操作')
+
+        }
+
+      }
+
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
